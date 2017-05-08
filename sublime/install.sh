e4af8eaafbffa999ac72bceef81175ca82c9b7b7
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-05-08 14:27:59 -0700

    Add sublime settings.

--- sublime/install.sh
+++ sublime/install.sh
@@ -11,7 +11,7 @@ set_sublime () {
     # Setup links
     cp "$dotfiles/sublime/Package Control.sublime-settings" "$sublime/Package Control.sublime-settings"
     # Execute the following after all packages are installed
-    echo "Copy Preferences.sublime-settings into `Sublime Text 3/Packages/User` to apply settings."
+    echo "Copy Preferences.sublime-settings into \"Sublime Text 3/Packages/User\" to apply settings."
     # cp "$dotfiles/sublime/Preferences.sublime-settings" "$sublime/Preferences.sublime-settings"
 }
 
