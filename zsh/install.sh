50a3281c71356256c580b2b7408663f6fa5aa1c7
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2018-07-15 18:19:53 -0700

    tmuxrepl addition

--- zsh/install.sh
+++ zsh/install.sh
@@ -17,16 +17,19 @@ setup () {
 
     # Clone oh-my-zsh
     git clone --depth 1 https://github.com/robbyrussell/oh-my-zsh.git $omz
-    
+
     # Clone zsh-autosuggestions
     git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $omz/custom/plugins/zsh-autosuggestions
 
     # Clone zsh-autocompletions
     git clone --depth 1 https://github.com/zsh-users/zsh-completions $omz/custom/plugins/zsh-completions
 
+    # Clone tmux-repl
+    git clone https://github.com/csurfer/tmuxrepl.git $omz/custom/plugins/tmuxrepl
+
     # Copy zshrc
     cp $zsh/zshrc ~/.zshrc
-    
+
     # Copy custom plugins
     cp -r $zsh/custom $omz
 }
